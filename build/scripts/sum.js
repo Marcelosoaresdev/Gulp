e350d4fe9f78cc3a2e67f90fb093174931c65437
Marcelo Soares <marcelohsoares142@gmail.com>
2024-06-28 15:58:09 -0300

    JavaScript e Images Minificadas com o Gulp

---
+++ build/scripts/sum.js
@@ -0,0 +1,1 @@
+function ಠ_ಠ379(o,r){return o+r}module.ಠ_ಠ380=ಠ_ಠ379;
--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,22 @@
 const gulp = require("gulp");
 const sass = require("gulp-sass")(require("sass"));
 const sourcemaps = require("gulp-sourcemaps");
+const uglify = require ("gulp-uglify");
+const obfuscate = require ("gulp-obfuscate");
+const imagemin = require ("gulp-imagemin");
+
+function comprimeImagens(){
+  return gulp.src('./source/images/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('./build/images'));
+}
+
+function comprimeJavaScript () {
+  return gulp.src('./source/scripts/*js')
+    .pipe(uglify())
+    .pipe(obfuscate()) // isso torna o arquivo ilegivel
+    .pipe(gulp.dest('./build/scripts'));
+}
 
 function compilaSass() {
   return gulp.src("./source/styles/main.scss") // Pega os arquivos da pasta
@@ -12,30 +28,11 @@ function compilaSass() {
     .pipe(gulp.dest("./build/styles")); // Salvamos os arquivos comprimidos e os sourcemaps na pasta styles dentro de build
 }
 
-function funcaoPadrao(callback) {
-  setTimeout(function () {
-    console.log("Executando via Gulp");
-    callback();
-  }, 2000);
-}
-
-function dizOi(callback) {
-  setTimeout(function () {
-    console.log("Ola, Gulp");
-    dizTchau(); // Tarefas privadas, são as que sao chamadas por outras functions
-    callback();
-  }, 1000);
-}
-
-function dizTchau() {
-  console.log("Tchau Gulp");
-}
-
 // exports.default = funcaoPadrao; //Tarefas publicas, são as que exportamos
 // exports.default = gulp.series(funcaoPadrao, dizOi); // Tarefa em serie, aguarda o fechamento de uma para a proxima se iniciar
-exports.default = gulp.parallel(funcaoPadrao, dizOi); // Tarefa em paralela, todas se iniciam juntas e fecham na respectiva ordem
-exports.dizOi = dizOi;
-exports.sass = compilaSass;
-exports.watch = function(){
+// exports.default = gulp.parallel(funcaoPadrao, dizOi); // Tarefa em paralela, todas se iniciam juntas e fecham na respectiva ordem
+exports.default = function(){
   gulp.watch('./source/styles/*.scss', {ignoreInitial: false }, gulp.series(compilaSass))
-}
+  gulp.watch('./source/scripts/*js', {ignoreInitial: false }, gulp.series(comprimeJavaScript))
+  gulp.watch('./source/images/*', {ignoreInitial: false }, gulp.series(comprimeImagens))
+};
